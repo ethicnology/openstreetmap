93640afb08e6a00b3f469cf967dc2903944dbc95
ethicnology <ethicnology@pm.me>
2025-05-19 10:04:29 -0400

    refactor: dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,7 @@ class MyApp extends StatelessWidget {
       create: (context) => getIt<MapCubit>()..loadMap(),
       child: MaterialApp(
         title: 'Map App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
+        theme: ThemeData.dark(),
         home: const MapPage(),
       ),
     );
